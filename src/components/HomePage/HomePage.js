1b23863db4e5e6b59d5ddd32d0271bffa9cb5975
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-04-10 18:16:10 +0300

    HomePage has been added

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './HomePage.module.css';
+import photo from '../../assets/images/photo.jpg'
+
+function HomePage() {
+    return (
+        <div className={s.homePage}>
+            <div className={s.text}>
+                <span>Hi there!</span>
+                <h1>I am Mary Grishchuk</h1>
+                <p>A Front-end Developer.</p>
+            </div>
+            <div className={s.photo}><img src={photo} alt="photo"/></div>
+        </div>
+    );
+}
+
+export default HomePage;
