f5f00bba70bbf2882d7cd75c7347a5210fabf619
Mary Grishchuk <mary.grishchuk1@gmail.com>
2021-04-14 13:10:33 +0300

    All fields required error added to ContactMe & Hire Me button restyled

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -10,20 +10,28 @@ function ContactMe() {
     let [name, setName] = useState(state.contactForm.name)
     let [email, setEmail] = useState(state.contactForm.email)
     let [messageText, setMessageText] = useState(state.contactForm.messageText)
+    let [error, setError] = useState("")
 
     let onNameInput = (e) => {
         setName(e.currentTarget.value)
+        setError("")
     }
     let onEmailInput = (e) => {
         setEmail(e.currentTarget.value)
+        setError("")
     }
     let onMessageInput = (e) => {
         setMessageText(e.currentTarget.value)
+        setError("")
     }
 
     let onSubmit = (e) => {
-        dispatch(onFormSubmit(name, email, messageText))
         e.preventDefault()
+        if (name & email & messageText) {
+            dispatch(onFormSubmit(name, email, messageText))
+        } else {
+            setError('All fields are required')
+        }
     }
 
     return (
@@ -31,10 +39,13 @@ function ContactMe() {
             <h2 className={s.title}>Contact Me</h2>
             <hr className={s.line}/>
             <form onSubmit={onSubmit} className={s.form}>
-                <input type="text" placeholder={"Name"} className={s.field} value={name} onChange={onNameInput}/>
-                <input type="email" placeholder={"E-mail"} className={s.field} value={email} onChange={onEmailInput}/>
-                <textarea className={`${s.field} ${s.textarea}`} placeholder={"Your message"} value={messageText}
+                <input type="text" placeholder={"*Name"} className={s.field} value={name} onChange={onNameInput}/>
+
+                <input type="email" placeholder={"*E-mail"} className={s.field} value={email}
+                       onChange={onEmailInput}/>
+                <textarea className={`${s.field} ${s.textarea}`} placeholder={"*Your message"} value={messageText}
                           onChange={onMessageInput}/>
+                <div className={s.error}>{error}</div>
                 <button className={s.btn}>SEND</button>
             </form>
         </div>
